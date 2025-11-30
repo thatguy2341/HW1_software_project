46eabc6b0e364071bd8b1a8f02ad653a3ce66e38
Noam-Avraham <noamavraham123@gmail.com>
2025-11-30 15:32:04 +0200

    update points+distance+add ans subtract vector function.

--- project.c
+++ project.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-# include <stdio.h>
-# include <stdlib.h>
+# include <math.h>
 
 struct cord
 {
@@ -17,8 +14,8 @@ struct vector
     struct cord *cords;
 };
 struct cluster{
-    struct vector point;
-    struct vector sum;
+    struct vector *point;
+    struct vector *sum;
     int points_assigned;
 }
 int main(int argc, char **argv)
@@ -75,35 +72,57 @@ int main(int argc, char **argv)
     struct vector* create_clusters()
     {
 
-    struct vector *clusters=malloc(sizeof(struct vector*)*k);
+    struct cluster *clusters=(struct cluster*)malloc(sizeof(struct cluster)*k);
     
 
     }
 
     //3 compute point distance from every point and update points index;
     //noam
-    void update_points(struct vector* clusters,struct vector* head_vec){
+    void update_points(struct cluster* clusters,struct vector* head_vec){
         struct vector* current;
         current=head_vec;
+
         while(current!=NULL){
-            int min;
+            
+            double min=distance(current,clusters[0].point);
             int current_cluster=current->cluster_index;
-            int next_cluster=current_cluster;
-            for(int i=0;i<k;i++){
-                int 
-                int temp=distance(current, &clusters[i]);
+            int next_cluster_index=0;
+            //check which cluster is the closest to the point
+            for(int i=1;i<k;i++){ 
+                double temp=distance(current, clusters[i].point);
                 if(temp<min){   
                     min=temp;
-                    next_cluster=i;
+                    next_cluster_index=i;
                 }
-            //update sum and counter of the clusters;
+            }
+            //update sum and counter of the clusters
+            if(current_cluster!=next_cluster_index){
+                current->cluster_index=next_cluster_index;
+                add_vector(clusters[current_cluster].sum,current,-1);
+                add_vector(clusters[next_cluster_index].sum,current,1);
+                clusters[current_cluster].points_assigned--;
+                clusters[next_cluster_index].points_assigned++;
+            }
+            //move to next point
+            current=current.next;
         }
-
+    //add two vectors from same size of linked list, operation is -1 for subtract 1 for add
+    void add_vector(struct vector* base,struct vector* add,int operation){
+        
+        struct cord *curr_base_cord=base->cords;
+        struct cord *curr_add_cord=add->cords;
+        
+        while(curr_base_cord!=NULL&&curr_add_cord!=NULL){
+            curr_base_cord->value+=curr_add_cord->value*(operation);
+            curr_base_cord=curr_base_cord->next;
+            curr_add_cord=curr_add_cord->next;
+        }
     }
     }
     //4
     //guy
-    int  update_clusters(struct vector* clusters,double epsilon){
+    int  update_clusters(struct cluster* clusters,double epsilon){
 
         return 0;
     }
@@ -112,8 +131,18 @@ int main(int argc, char **argv)
     //5 distance between points
     //noam
     double distance(struct vector* x,struct vector* y){
-
-            return distance;
+        double distance=0;
+        double sum_square=0;
+        struct cord *x_current_cord=x->cords;
+        struct cord *y_current_cord=y->cords;
+        while (y_current_cord!=NULL&&x_current_cord!=NULL){
+            double diff=(x_current_cord->value)-(y_current_cord->value);
+            sum_square+=diff*diff;
+            y_current_cord=y_current_cord->next;
+            x_current_cord=x_current_cord->next
+        }
+        distance=sqrt(sum_square);
+        return distance;
     }
 
  
